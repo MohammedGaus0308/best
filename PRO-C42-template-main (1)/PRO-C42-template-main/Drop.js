437c71381d913ca9da2fe7bce85536ddfba0ba41
MohammedGaus0308 <74346793+MohammedGaus0308@users.noreply.github.com>
2021-05-10 14:59:49 +0530

    Add files via upload

---
+++ PRO-C42-template-main (1)/PRO-C42-template-main/Drop.js
@@ -0,0 +1,24 @@
+class Drop{
+    constructor(x,y){
+        var options={
+            'restitution ': 0.1,
+            'friction' : 0.1
+        }
+        this.rain=Bodies.circle(x,y,5,options)
+        this.radius=5
+        World.add(world,this.body)
+    }
+
+    update(){
+        if(this.rain.position.y>height){
+Matter.Body.setPosition(this.rain,{x : random(0,400), y : random(0,400)})
+        }
+    }
+
+    display(){
+        noStroke()
+        fill(0,0,255)
+        ellipseMode(RADIUS)
+        ellipes(this.rain.body.position.x,this.rain.body.position.y,this.radius,this.radius)
+    }
+}
